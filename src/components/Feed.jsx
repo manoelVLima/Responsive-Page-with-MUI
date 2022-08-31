8cbb0d3d207700370c4a560eee53bd32ac0be4ba
Manoel Vieira Lima Junior <manoel.lima03@gmail.com>
2022-08-31 18:00:32 -0300

    Implementei um boxshadow

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -10,7 +10,7 @@ function Feed() {
     }
   }))
   return (
-    <StyledBox p={2} flex={3}>
+    <StyledBox p={2} flex={2}>
       <Posts img="https://source.unsplash.com/random/450x450?r=213"/>
       <Posts img="https://source.unsplash.com/random/450x450?r=21" />
       <Posts img="https://source.unsplash.com/random/450x450?r=67" />
